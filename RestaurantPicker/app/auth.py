7416ef41d9ec443435c7c0bbcce4119ad03faf2b
AlyanaLeandry <83033312+AlyanaLeandry@users.noreply.github.com>
2022-10-09 08:58:32 -0400

    Update auth.py

--- RestaurantPicker/app/auth.py
+++ RestaurantPicker/app/auth.py
@@ -1,1 +1,6 @@
 # this will be used to render the pages used in our login system
+import functools
+from flask import Blueprint
+from flask import redirect
+from flask import render_template
+from flask import session
