78756d4e94909377d0753334eb8ea025e971c17f
AlyanaLeandry <83033312+AlyanaLeandry@users.noreply.github.com>
2022-10-09 09:03:20 -0400

    Update views.py

--- RestaurantPicker/app/views.py
+++ RestaurantPicker/app/views.py
@@ -1,1 +1,3 @@
 # this is used to render our routes (randomizer page, map page, etc...)
+from flask import render_template, redirect, request, current_app, session, flash, url_for
+from flask.ext.secuirty import LoginForm, current_user, login_required, login_ser
