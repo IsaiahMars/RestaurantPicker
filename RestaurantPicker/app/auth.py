01c8fc7281e65d7d5a8a70aa4aa008c757671d56
AlyanaLeandry <83033312+AlyanaLeandry@users.noreply.github.com>
2022-10-11 08:13:40 -0400

    Update auth.py

--- RestaurantPicker/app/auth.py
+++ RestaurantPicker/app/auth.py
@@ -1,6 +1,8 @@
 # this will be used to render the pages used in our login system
-import functools
-from flask import Blueprint
-from flask import redirect
-from flask import render_template
-from flask import session
+import os
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from azure.storage.blob import BlobServiceClient
+from .models import User
+from . import db
