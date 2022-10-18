e34db7fa70153c5d8afbc40963a7b91b0eb29f89
IsaiahMars <93736007+IsaiahMars@users.noreply.github.com>
2022-10-18 12:42:04 -0400

    Example 'restaurant' page
    Made each business in the home page link to a restaurant page which displays basic info.

--- python yelp/app/views.py
+++ python yelp/app/views.py
@@ -8,25 +8,25 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        location = request.form.get("location")        # First row of 'Trending' restaurants
+        location = request.form.get("location")
         PARAMETERS = {'location':location,
               'radius':2500,
               'limit':10,
               'sort_by': 'rating',
               'term':'restaurant'
             }
-        response = requests.get(url=BUSINESS_PATH, 
+        response = requests.get(url=BUSINESS_SEARCH, 
                                 params=PARAMETERS, 
                                 headers=HEADERS)
         parsed = json.loads(response.text)
         trendingBusinesses = parsed["businesses"]
 
-        PARAMETERS = {'location':location,              # Second row of 'Top 50'/'All nearby' restaurants
-              'radius':2500,                            # Max request limit for yelp fusion api is 50, default is 20.
+        PARAMETERS = {'location':location,
+              'radius':2500,
               'limit': 50,
               'term':'restaurant'
             }
-        response = requests.get(url=BUSINESS_PATH, 
+        response = requests.get(url=BUSINESS_SEARCH, 
                                 params=PARAMETERS, 
                                 headers=HEADERS)
         parsed = json.loads(response.text)
@@ -36,20 +36,29 @@ def home():
 
     return render_template("home.html", trendingBusinesses=[], allBusinesses=[])
 
-@views.route('/restaurant', methods=["POST", "GET"])   
-def restaurant():
-    return render_template("restaurant.html") 
+@views.route('/restaurant/<businessId>', methods=["POST", "GET"])   
+def restaurant(businessId):
+    if request.method == 'GET':
+        PARAMETERS = {}
+        response = requests.get(url=BUSINESS_DETAILS + businessId, 
+                                params=PARAMETERS, 
+                                headers=HEADERS)
+        parsed = json.loads(response.text)
+        print("parsed data ---------------- ", flush=True)
+        print(parsed, flush=True)
+        return render_template("restaurant.html", id=businessId, business=parsed) 
 
 
 
 
 # link to tutorial: https://www.youtube.com/watch?v=oggMBtza80E
-# link to code source used in tutorial: https://github.com/areed1192/sigma_coding_youtube/blob/master/python/python-api/yelp-api/Yelp%20API%20-%20Other%20Search%20%26%20Categories.py
+# link to code source: https://github.com/areed1192/sigma_coding_youtube/blob/master/python/python-api/yelp-api/Yelp%20API%20-%20Other%20Search%20%26%20Categories.py
 # link to parsing example: https://python.gotrained.com/yelp-fusion-api-tutorial/
 
 # Define API Key, Search Type, and header
 MY_API_KEY = 'YOUR_API_KEY_HERE'
-BUSINESS_PATH = 'https://api.yelp.com/v3/businesses/search'
+BUSINESS_SEARCH = 'https://api.yelp.com/v3/businesses/search'
+BUSINESS_DETAILS = 'https://api.yelp.com/v3/businesses/'
 HEADERS = {'Authorization': 'bearer %s' % MY_API_KEY}
 
 
