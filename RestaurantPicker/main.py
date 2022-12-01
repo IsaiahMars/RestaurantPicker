906db1fe6d245ba29bc4101f85d6cb996b59ec05
IsaiahMars <93736007+IsaiahMars@users.noreply.github.com>
2022-12-01 15:26:23 -0500

    Deployment Testing 3

--- RestaurantPicker/main.py
+++ RestaurantPicker/main.py
@@ -1,7 +1,10 @@
 from app import createApp
+from waitress import serve
+from os import environ
+
 
 app = createApp()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
-
+    #app.run(host="0.0.0.0", port=int(environ.get("PORT", 8080)), debug=True)
+    serve(app, host='0.0.0.0', port=int(environ.get("PORT", 8080)))
