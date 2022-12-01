1614f37c02f6cc45c39fbb4065821b064427d753
IsaiahMars <93736007+IsaiahMars@users.noreply.github.com>
2022-11-30 20:39:47 -0500

    Deployment Testing 2

--- RestaurantPicker/main.py
+++ RestaurantPicker/main.py
@@ -3,10 +3,5 @@ from app import createApp
 app = createApp()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
-
-# This is the file used to run our website. In visual studio code,
-# click the sideways triangle in the top right corner of your screen,
-# wait a few seconds, and ctrl + click on the link provided. This is a
-# locally hosted webpage.
